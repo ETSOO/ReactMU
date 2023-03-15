35be3e362c904129a9ed5446867e401fb9b48e6c
Garry Xiao <xz@etsoo.com>
2023-03-15 16:33:42 +1300

    Improve InputField

--- src/InputField.tsx
+++ src/InputField.tsx
@@ -1,21 +1,28 @@
-import { useDelayedExecutor } from '@etsoo/react';
-import { TextField, TextFieldProps } from '@mui/material';
-import React from 'react';
-import { MUGlobal } from './MUGlobal';
+import { useDelayedExecutor } from "@etsoo/react";
+import { TextField, TextFieldProps } from "@mui/material";
+import React from "react";
+import { MUGlobal } from "./MUGlobal";
 
 /**
  * Input field props
  */
 export type InputFieldProps = TextFieldProps & {
-    /**
-     * Change delay (ms) to avoid repeatly dispatch onChange
-     */
-    changeDelay?: number;
+  /**
+   * Change delay (ms) to avoid repeatly dispatch onChange
+   */
+  changeDelay?: number;
 
-    /**
-     * Is the field read only?
-     */
-    readOnly?: boolean;
+  /**
+   * Change delay handler, without it onChange will be applied
+   */
+  onChangeDelay?: React.ChangeEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  >;
+
+  /**
+   * Is the field read only?
+   */
+  readOnly?: boolean;
 };
 
 /**
@@ -24,62 +31,63 @@ export type InputFieldProps = TextFieldProps & {
  * @returns Component
  */
 export const InputField = React.forwardRef<HTMLDivElement, InputFieldProps>(
-    (props, ref) => {
-        // Destruct
-        const {
-            changeDelay,
-            InputLabelProps = {},
-            InputProps = {},
-            onChange,
-            readOnly,
-            size = MUGlobal.inputFieldSize,
-            variant = MUGlobal.inputFieldVariant,
-            ...rest
-        } = props;
+  (props, ref) => {
+    // Destruct
+    const {
+      changeDelay,
+      InputLabelProps = {},
+      InputProps = {},
+      onChange,
+      onChangeDelay,
+      readOnly,
+      size = MUGlobal.inputFieldSize,
+      variant = MUGlobal.inputFieldVariant,
+      ...rest
+    } = props;
 
-        // Shrink
-        InputLabelProps.shrink ??= MUGlobal.searchFieldShrink;
+    // Shrink
+    InputLabelProps.shrink ??= MUGlobal.searchFieldShrink;
 
-        // Read only
-        if (readOnly != null) InputProps.readOnly = readOnly;
+    // Read only
+    if (readOnly != null) InputProps.readOnly = readOnly;
 
-        const isMounted = React.useRef(true);
-        const delayed =
-            onChange != null && changeDelay != null && changeDelay >= 1
-                ? useDelayedExecutor(onChange, changeDelay)
-                : undefined;
+    const isMounted = React.useRef(true);
+    const createDelayed = () => {
+      if (changeDelay != null && changeDelay >= 1) {
+        const changeHandler = onChangeDelay ?? onChange;
+        if (changeHandler)
+          return useDelayedExecutor(changeHandler, changeDelay);
+      }
+      return undefined;
+    };
+    const delayed = createDelayed();
 
-        const onChangeEx = (
-            event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-        ) => {
-            if (onChange == null) return;
+    const onChangeEx = (
+      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+      if (onChange && (delayed == null || onChangeDelay != null))
+        onChange(event);
+      delayed?.call(undefined, event);
+    };
 
-            if (changeDelay == null || changeDelay < 1) {
-                onChange(event);
-                return;
-            }
+    React.useEffect(() => {
+      return () => {
+        isMounted.current = false;
+        delayed?.clear();
+      };
+    }, []);
 
-            delayed?.call(undefined, event);
-        };
-
-        React.useEffect(() => {
-            return () => {
-                isMounted.current = false;
-                delayed?.clear();
-            };
-        }, []);
-
-        // Layout
-        return (
-            <TextField
-                ref={ref}
-                InputLabelProps={InputLabelProps}
-                InputProps={InputProps}
-                onChange={onChangeEx}
-                size={size}
-                variant={variant}
-                {...rest}
-            />
-        );
-    }
+    // Layout
+    return (
+      <TextField
+        ref={ref}
+        InputLabelProps={InputLabelProps}
+        InputProps={InputProps}
+        onChange={onChangeEx}
+        size={size}
+        variant={variant}
+        {...rest}
+      />
+    );
+  }
 );
