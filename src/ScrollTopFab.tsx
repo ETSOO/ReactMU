09ac6575abfec797012e9ad0f38a820dc7e414a6
Garry Xiao <xz@etsoo.com>
2023-04-15 12:10:49 +1200

    Improve ScrollTopFab

--- src/ScrollTopFab.tsx
+++ src/ScrollTopFab.tsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import { Fab, useScrollTrigger, Zoom } from '@mui/material';
-import VerticalAlignTopIcon from '@mui/icons-material/VerticalAlignTop';
-import { CustomFabProps } from './CustomFabProps';
+import React from "react";
+import { Fab, useScrollTrigger, Zoom } from "@mui/material";
+import VerticalAlignTopIcon from "@mui/icons-material/VerticalAlignTop";
+import { CustomFabProps } from "./CustomFabProps";
 
 /**
  * Scroll to top fab
  * @returns Component
  */
 export function ScrollTopFab(props: CustomFabProps) {
-    // Destruct
-    const { color, size, target, title } = props;
+  // Destruct
+  const { color, size, target, title } = props;
 
-    // Scroll trigger
-    const trigger = useScrollTrigger({
-        target,
-        disableHysteresis: true,
-        threshold: 120
-    });
+  // Scroll trigger
+  const trigger = useScrollTrigger({
+    target,
+    disableHysteresis: true,
+    threshold: 120
+  });
 
-    // Icon click handler
-    // behavior: 'smooth'
-    const handleClick = () => {
-        target.scrollTo({ top: 0 });
-    };
+  // Icon click handler
+  // behavior: 'smooth'
+  const handleClick = () => {
+    target.scrollTo({ top: 0 });
+  };
 
-    return (
-        <Zoom in={trigger}>
-            <Fab color={color} size={size} title={title} onClick={handleClick}>
-                <VerticalAlignTopIcon />
-            </Fab>
-        </Zoom>
-    );
+  return trigger ? (
+    <Zoom in={trigger}>
+      <Fab color={color} size={size} title={title} onClick={handleClick}>
+        <VerticalAlignTopIcon />
+      </Fab>
+    </Zoom>
+  ) : (
+    <React.Fragment />
+  );
 }
