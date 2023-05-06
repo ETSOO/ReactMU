b629e37a9afbd1a7a1e0fcf73e56fccbeff9f457
Garry Xiao <xz@etsoo.com>
2023-05-06 17:45:33 +1200

    Improve ServiceApp

--- src/app/IServiceUser.ts
+++ src/app/IServiceUser.ts
@@ -1,17 +1,22 @@
-import { IActionResult, IUser } from '@etsoo/appscript';
+import { IActionResult, IUser } from "@etsoo/appscript";
 
 /**
  * Service user interface
  */
 export interface IServiceUser extends IUser {
-    /**
-     * Service device id
-     */
-    serviceDeviceId: string;
+  /**
+   * Service device id
+   */
+  serviceDeviceId: string;
+
+  /**
+   * Service passphrase encrypted
+   */
+  servicePassphrase: string;
 }
 
 /**
  * Service user login result
  */
 export type ServiceLoginResult<U extends IServiceUser = IServiceUser> =
-    IActionResult<U>;
+  IActionResult<U>;
