a956a7a185e9929326523ee2432aadd29712e6c3
Garry Xiao <xz@etsoo.com>
2023-08-30 13:33:08 +1200

    Improve FileUploadButton

--- src/FileUploadButton.tsx
+++ src/FileUploadButton.tsx
@@ -6,12 +6,29 @@ import React from "react";
  */
 export type FileUploadButtonProps = ButtonProps<"label"> & {
   /**
+   * Max files allowed
+   */
+  maxFiles?: number;
+
+  /**
+   * Max file size
+   */
+  maxFileSize?: number;
+
+  /**
    * Input field attributes
    */
   inputProps?: Omit<
     React.InputHTMLAttributes<HTMLInputElement>,
     "type" | "hidden"
   >;
+
+  /**
+   * File invalid handler
+   * @param values [expected, actual]
+   * @param file File
+   */
+  onFileInvalid?: (values: [number, number], file?: File) => void;
 
   /**
    * Upload files callback
@@ -27,7 +44,15 @@ export type FileUploadButtonProps = ButtonProps<"label"> & {
  */
 export function FileUploadButton(props: FileUploadButtonProps) {
   // Destruct
-  const { inputProps, onUploadFiles, children, ...rest } = props;
+  const {
+    maxFiles,
+    maxFileSize,
+    inputProps,
+    onFileInvalid,
+    onUploadFiles,
+    children,
+    ...rest
+  } = props;
 
   const { onChange } = inputProps ?? {};
 
@@ -44,7 +69,27 @@ export function FileUploadButton(props: FileUploadButtonProps) {
 
           if (onUploadFiles) {
             const files = event.target.files;
-            if (files == null || files.length == 0) return;
+            if (files == null) return;
+
+            const fl = files.length;
+            if (fl === 0) return;
+
+            if (maxFiles && maxFiles > 0 && fl > maxFiles) {
+              if (onFileInvalid) onFileInvalid([maxFiles, fl]);
+              return;
+            }
+
+            if (maxFileSize && maxFileSize > 0) {
+              for (let f = 0; f < fl; f++) {
+                const file = files[f];
+                if (file.size > maxFileSize) {
+                  if (onFileInvalid)
+                    onFileInvalid([maxFileSize, file.size], file);
+                  return;
+                }
+              }
+            }
+
             onUploadFiles(files);
           }
         }}
