4964c19138230b737a2b7ff6b7ef25c39bea2136
Garry Xiao <xz@etsoo.com>
2025-01-09 21:53:29 +1300

    Improve IServiceApp

--- src/app/IServiceApp.ts
+++ src/app/IServiceApp.ts
@@ -1,6 +1,7 @@
-import { ApiRefreshTokenDto, IApi } from "@etsoo/appscript";
+import { ApiRefreshTokenDto, IApi, IApiPayload } from "@etsoo/appscript";
 import { ReactAppType } from "./ReactApp";
 import { IServiceUser, ServiceUserToken } from "./IServiceUser";
+import { IActionResult } from "@etsoo/shared";
 
 /**
  * Service application interface
@@ -18,20 +19,31 @@ export interface IServiceApp extends ReactAppType {
 
   /**
    * Load core system UI
+   * @param tryLogin Try login or not
    */
-  loadCore(): void;
+  loadCore(tryLogin?: boolean): void;
+
+  /**
+   * Switch organization
+   * @param organizationId Organization ID
+   * @param fromOrganizationId From organization ID
+   * @param payload Payload
+   */
+  switchOrg(
+    organizationId: number,
+    fromOrganizationId?: number,
+    payload?: IApiPayload<IActionResult<IServiceUser & ServiceUserToken>>
+  ): Promise<IActionResult<IServiceUser & ServiceUserToken> | undefined>;
 
   /**
    *
    * @param user Current user
    * @param core Core system API token data
-   * @param keep Keep in local storage or not
    * @param dispatch User state dispatch
    */
   userLoginEx(
     user: IServiceUser & ServiceUserToken,
     core?: ApiRefreshTokenDto,
-    keep?: boolean,
     dispatch?: boolean
   ): void;
 }
