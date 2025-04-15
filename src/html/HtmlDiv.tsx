dd86b045285ca2687cdfdd914dc76228dcfb7c92
Garry Xiao <xz@etsoo.com>
2025-04-15 12:44:00 +1200

    Improve HtmlDiv

--- src/html/HtmlDiv.tsx
+++ src/html/HtmlDiv.tsx
@@ -1,8 +1,29 @@
 import { Utils } from "@etsoo/shared";
-import DOMPurify from "dompurify";
 import { HTMLAttributes } from "react";
 
 class HtmlDivElement extends HTMLElement {
+  static get observedAttributes() {
+    return ["displaystyle"];
+  }
+
+  private wrapper: HTMLDivElement | null = null;
+  private observer: MutationObserver | null = null;
+
+  private _displayStyle?: string = undefined;
+
+  /**
+   * Display style
+   */
+  get displayStyle() {
+    return this._displayStyle;
+  }
+  set displayStyle(style: string | undefined) {
+    this._displayStyle = style;
+    if (this.wrapper && style != null) {
+      this.wrapper.style.cssText = style;
+    }
+  }
+
   constructor() {
     super();
   }
@@ -13,20 +34,55 @@ class HtmlDivElement extends HTMLElement {
 
     // Create a wrapper element to hold the sanitized HTML content
     const wrapper = document.createElement("div");
+    wrapper.style.cssText = this.displayStyle ?? "";
+    shadow.appendChild(wrapper);
+
+    this.wrapper = wrapper;
+    this.setContent();
+
+    let miliseconds = Date.now();
+    this.observer = new MutationObserver(() => {
+      const m = Date.now();
+      if (m - miliseconds < 50) return; // Ignore fast changes
+      miliseconds = m;
+      this.setContent();
+    });
+    this.observer.observe(this, {
+      childList: true,
+      subtree: true,
+      characterData: true
+    });
+  }
+
+  disconnectedCallback() {
+    if (this.observer) {
+      this.observer.disconnect();
+      this.observer = null;
+    }
+  }
+
+  attributeChangedCallback(name: string, _oldValue: any, newValue: any) {
+    if (name === "displaystyle" && typeof newValue === "string") {
+      this.displayStyle = newValue;
+    }
+  }
+
+  setContent() {
+    const wrapper = this.wrapper;
+    if (!wrapper) return;
+
     const html = this.innerHTML;
     if (
       Utils.hasHtmlEntity(html) &&
       !Utils.hasHtmlTag(html) &&
       this.textContent
     ) {
-      wrapper.innerHTML = DOMPurify.sanitize(this.textContent);
+      wrapper.innerHTML = this.textContent;
     } else {
-      wrapper.innerHTML = DOMPurify.sanitize(html);
+      wrapper.innerHTML = html;
     }
 
     this.textContent = null; // Clear the textContent to avoid duplication
-
-    shadow.appendChild(wrapper);
   }
 }
 
@@ -39,7 +95,21 @@ if (!customElements.get("html-div")) {
  * Custom HTML element properties
  * 自定义 HTML 元素属性
  */
-export type HtmlDivProps = HTMLAttributes<HTMLElement>;
+export type HtmlDivProps = HTMLAttributes<HTMLElement> & {
+  /**
+   * Display style
+   * 显示样式
+   */
+  displayStyle?: string;
+};
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "html-div": React.HTMLAttributes<HTMLElement>;
+    }
+  }
+}
 
 /**
  * Custom HTML element that sanitizes and displays HTML content
