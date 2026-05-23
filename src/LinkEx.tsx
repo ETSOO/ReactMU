31421f612e55868a580ad49e41ab589b0e812ea7
Garry Xiao <xz@etsoo.com>
2026-05-23 09:19:55 +0800

    Add disabled to LinkEx

--- src/LinkEx.tsx
+++ src/LinkEx.tsx
@@ -1,10 +1,16 @@
 import { Link, LinkProps } from "@mui/material";
+import React from "react";
 import { Link as RouterLink } from "react-router";
 
 /**
  * Extended link component props
  */
-export type LinkExProps = Omit<LinkProps<typeof RouterLink>, "component">;
+export type LinkExProps = Omit<LinkProps<typeof RouterLink>, "component"> & {
+  /**
+   * Disabled or not
+   */
+  disabled?: boolean;
+};
 
 /**
  * Extended link component
@@ -12,6 +18,12 @@ export type LinkExProps = Omit<LinkProps<typeof RouterLink>, "component">;
  * @returns Component
  */
 export function LinkEx(props: LinkExProps) {
-  const { underline = "hover", ...rest } = props;
-  return <Link component={RouterLink} underline={underline} {...rest} />;
+  const { children, disabled, underline = "hover", ...rest } = props;
+  return disabled ? (
+    <React.Fragment>{children}</React.Fragment>
+  ) : (
+    <Link component={RouterLink} underline={underline} {...rest}>
+      {children}
+    </Link>
+  );
 }
